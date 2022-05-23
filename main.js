e871697ff700978618169770ea7f528977726ddf
Aryan Arkhit Behra <notarkhit@gmail.com>
2022-05-24 03:36:17 +0530

    Added stars and background

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' ;
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75 , window.innerWidth / window.innerHeight , 0.01 , 1000) ;
@@ -30,8 +32,27 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper)
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper,gridHelper)
 
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread( 100) );
+
+  star.position.set(x ,y , z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
 
 
 function animate() {
@@ -41,6 +62,7 @@ function animate() {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  controls.update();
 
   renderer.render( scene, camera );
 }
